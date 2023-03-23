28e1fb71e4d45e80c0a6822afdba67ce5d7ff4c2
aitor.molero <aitor.molero@opendeusto.es>
2023-03-23 19:58:33 +0100

    reservar local

---
+++ reservar_local.h
@@ -0,0 +1,15 @@
+#ifndef _RESERVAR_LOCAL_H_
+#define _RESERVAR_LOCAL_H_
+
+void reservarLocal();
+
+char mostrarListado();
+void cargarLocales();
+
+char* elegirFecha();
+char* elegirDiscoteca();
+
+void pagarReserva();
+char mostrarPagarReserva();
+
+#endif
