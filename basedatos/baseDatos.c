c4ed118bb4b7c45b394f144db1d6d25d1a9ec4c5
Endika <e.blanco@opendeusto.es>
2023-03-23 20:13:18 +0100

    exists DB

--- basedatos/baseDatos.c
+++ basedatos/baseDatos.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <sqlite/sqlite3.h>
 
+int db_existe(char *nombrearch) {
+    FILE *arch = fopen(nombrearch,"r");
+    if (arch) {
+        fclose(arch);
+        return 1;
+    }
+    return 0;
+}
+
+
 int main(int argc, char* argv[])
 {
     sqlite3 *db;
-    int rc = sqlite3_open("test.db", &db);
+    int db_exists = db_existe("basedatos.db");
+    int rc = sqlite3_open("basedatos.db", &db);
+
     if (rc != SQLITE_OK) {
         fprintf(stderr, "No se puede abrir la base de datos: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
@@ -12,6 +24,9 @@ int main(int argc, char* argv[])
     }
 
     printf("La base de datos se ha creado o abierto con éxito.\n");
+    if (!db_exists) {
+        // Implementacion de importacion de datos CSV
+    }
     sqlite3_close(db);
     return 0;
 }
