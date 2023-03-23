b1ae00c06b6cd2facb3aa00ddfa914e776512cb2
Endika <e.blanco@opendeusto.es>
2023-03-23 21:00:10 +0100

    fix nombre variable

--- basedatos/baseDatos.c
+++ basedatos/baseDatos.c
@@ -75,8 +75,9 @@ int main(int argc, char * argv[]) {
     }
 
     fclose(fp);
-    char * sql = "CREATE TABLE dj(identificador INT PRIMARY KEY NOT NULL,nombre_comercial TEXT NOT NULL,genero_musical TEXT NOT NULL,fecha_nacimiento TEXT NOT NULL,numero_contacto TEXT NOT NULL);";
-    rc = sqlite3_exec(db, sql, 0, 0, & err_msg);
+
+    char * sql2 = "CREATE TABLE dj(identificador INT PRIMARY KEY NOT NULL,nombre_comercial TEXT NOT NULL,genero_musical TEXT NOT NULL,fecha_nacimiento TEXT NOT NULL,numero_contacto TEXT NOT NULL);";
+    rc = sqlite3_exec(db, sql2, 0, 0, & err_msg);
     if (rc != SQLITE_OK) {
       fprintf(stderr, "SQL error: %s\n", err_msg);
       sqlite3_free(err_msg);
@@ -84,21 +85,21 @@ int main(int argc, char * argv[]) {
       return 1;
     }
 
-    FILE * fp = fopen("dj.csv", "r");
-    if (!fp) {
+    FILE * fp2 = fopen("dj.csv", "r");
+    if (!fp2) {
       printf("No se puede abrir el archivo\n");
       return 1;
     }
 
-    char line[1024];
+    char line2[1024];
 
-    fgets(line, sizeof(line), fp); // Saltar la primera línea (encabezados)
+    fgets(line2, sizeof(line2), fp2); // Saltar la primera línea (encabezados)
 
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line2, sizeof(line2), fp2)) {
 
       char identificador[50], nombre_comercial[50], genero_musical[50], fecha_nacimiento[50], numero_contacto[50];
 
-      sscanf(line, "%[^','],%[^','],%[^','],%[^','],%s", identificador,
+      sscanf(line2, "%[^','],%[^','],%[^','],%[^','],%s", identificador,
         nombre_comercial,
         genero_musical,
         fecha_nacimiento,
@@ -123,10 +124,10 @@ int main(int argc, char * argv[]) {
       }
     }
 
-    fclose(fp);
+    fclose(fp2);
 
-    char * sql = "CREATE TABLE dj(identificador INT PRIMARY KEY NOT NULL,nombre_comercial TEXT NOT NULL,genero_musical TEXT NOT NULL,fecha_nacimiento TEXT NOT NULL,numero_contacto TEXT NOT NULL);";
-    rc = sqlite3_exec(db, sql, 0, 0, & err_msg);
+    char * sql3 = "CREATE TABLE listaeventos(Dia TEXT NOT NULL,Descripcion TEXT NOT NULL,nombre_discoteca TEXT NOT NULL,Aforo INT NOT NULL);";
+    rc = sqlite3_exec(db, sql3, 0, 0, & err_msg);
     if (rc != SQLITE_OK) {
       fprintf(stderr, "SQL error: %s\n", err_msg);
       sqlite3_free(err_msg);
@@ -134,70 +135,21 @@ int main(int argc, char * argv[]) {
       return 1;
     }
 
-    FILE * fp = fopen("dj.csv", "r");
-    if (!fp) {
+    FILE * fp3 = fopen("listaeventos.csv", "r");
+    if (!fp3) {
       printf("No se puede abrir el archivo\n");
       return 1;
     }
 
-    char line[1024];
+    char line3[1024];
 
-    fgets(line, sizeof(line), fp); // Saltar la primera línea (encabezados)
+    fgets(line3, sizeof(line3), fp3); // Saltar la primera línea (encabezados)
 
-    while (fgets(line, sizeof(line), fp)) {
-
-      char identificador[50], nombre_comercial[50], genero_musical[50], fecha_nacimiento[50], numero_contacto[50];
-
-      sscanf(line, "%[^','],%[^','],%[^','],%[^','],%s", identificador,
-        nombre_comercial,
-        genero_musical,
-        fecha_nacimiento,
-        numero_contacto);
-
-      char sql_insert[1024];
-
-      sprintf(sql_insert, "INSERT INTO dj VALUES(%s,'%s','%s','%s','%s');",
-        identificador,
-        nombre_comercial,
-        genero_musical,
-        fecha_nacimiento,
-        numero_contacto);
-
-      rc = sqlite3_exec(db, sql_insert, 0, 0, & err_msg);
-
-      if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        break;
-
-      }
-    }
-
-    fclose(fp);
-    char * sql = "CREATE TABLE listaeventos(Dia TEXT NOT NULL,Descripcion TEXT NOT NULL,nombre_discoteca TEXT NOT NULL,Aforo INT NOT NULL);";
-    rc = sqlite3_exec(db, sql, 0, 0, & err_msg);
-    if (rc != SQLITE_OK) {
-      fprintf(stderr, "SQL error: %s\n", err_msg);
-      sqlite3_free(err_msg);
-      sqlite3_close(db);
-      return 1;
-    }
-
-    FILE * fp = fopen("listaeventos.csv", "r");
-    if (!fp) {
-      printf("No se puede abrir el archivo\n");
-      return 1;
-    }
-
-    char line[1024];
-
-    fgets(line, sizeof(line), fp); // Saltar la primera línea (encabezados)
-
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line3, sizeof(line3), fp3)) {
 
       char Dia[50], Descripcion[50], nombre_discoteca[50], Aforo[50];
 
-      sscanf(line, "%[^','],%[^','],%[^','],%s", Dia,
+      sscanf(line3, "%[^','],%[^','],%[^','],%s", Dia,
         Descripcion,
         nombre_discoteca,
         Aforo);
@@ -220,10 +172,10 @@ int main(int argc, char * argv[]) {
       }
     }
 
-    fclose(fp);
+    fclose(fp3);
 
-    char * sql = "CREATE TABLE rrpp(Codigo INT PRIMARY KEY NOT NULL,nombre TEXT NOT NULL,zona_recogida TEXT NOT NULL,hora_recogida TEXT NOT NULL,numero_contacto TEXT NOT NULL);";
-    rc = sqlite3_exec(db, sql, 0, 0, & err_msg);
+    char * sql4 = "CREATE TABLE rrpp(Codigo INT PRIMARY KEY NOT NULL,nombre TEXT NOT NULL,zona_recogida TEXT NOT NULL,hora_recogida TEXT NOT NULL,numero_contacto TEXT NOT NULL);";
+    rc = sqlite3_exec(db, sql4, 0, 0, & err_msg);
     if (rc != SQLITE_OK) {
       fprintf(stderr, "SQL error: %s\n", err_msg);
       sqlite3_free(err_msg);
@@ -231,21 +183,21 @@ int main(int argc, char * argv[]) {
       return 1;
     }
 
-    FILE * fp = fopen("rrpp.csv", "r");
-    if (!fp) {
+    FILE * fp4 = fopen("rrpp.csv", "r");
+    if (!fp4) {
       printf("No se puede abrir el archivo\n");
       return 1;
     }
 
-    char line[1024];
+    char line4[1024];
 
-    fgets(line, sizeof(line), fp); // Saltar la primera línea (encabezados)
+    fgets(line4, sizeof(line4), fp4); // Saltar la primera línea (encabezados)
 
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line4, sizeof(line4), fp4)) {
 
       char Codigo[50], nombre[50], zona_recogida[50], hora_recogida[50], numero_contacto[50];
 
-      sscanf(line, "%[^','],%[^','],%[^','],%[^','],%s", Codigo,
+      sscanf(line4, "%[^','],%[^','],%[^','],%[^','],%s", Codigo,
         nombre,
         zona_recogida,
         hora_recogida,
@@ -270,10 +222,10 @@ int main(int argc, char * argv[]) {
       }
     }
 
-    fclose(fp);
+    fclose(fp4);
 
-    char * sql = "CREATE TABLE usuarios(Nombre TEXT NOT NULL,nombre_usuario TEXT NOT NULL,Sexo TEXT NOT NULL,Edad INT NOT NULL,email TEXT NOT NULL,password TEXT NOT NULL);";
-    rc = sqlite3_exec(db, sql, 0, 0, & err_msg);
+    char * sql5 = "CREATE TABLE usuarios(Nombre TEXT NOT NULL,nombre_usuario TEXT NOT NULL,Sexo TEXT NOT NULL,Edad INT NOT NULL,email TEXT NOT NULL,password TEXT NOT NULL);";
+    rc = sqlite3_exec(db, sql5, 0, 0, & err_msg);
     if (rc != SQLITE_OK) {
       fprintf(stderr, "SQL error: %s\n", err_msg);
       sqlite3_free(err_msg);
@@ -281,21 +233,21 @@ int main(int argc, char * argv[]) {
       return 1;
     }
 
-    FILE * fp = fopen("usuarios.csv", "r");
-    if (!fp) {
+    FILE * fp5 = fopen("usuarios.csv", "r");
+    if (!fp5) {
       printf("No se puede abrir el archivo\n");
       return 1;
     }
 
-    char line[1024];
+    char line5[1024];
 
-    fgets(line, sizeof(line), fp); // Saltar la primera línea (encabezados)
+    fgets(line5, sizeof(line5), fp5); // Saltar la primera línea (encabezados)
 
-    while (fgets(line, sizeof(line), fp)) {
+    while (fgets(line5, sizeof(line5), fp5)) {
 
       char Nombre[50], nombre_usuario[50], Sexo[50], Edad[50], email[50], password[50];
 
-      sscanf(line, "%[^','],%[^','],%[^','],%[^','],%[^','],%s", Nombre,
+      sscanf(line5, "%[^','],%[^','],%[^','],%[^','],%[^','],%s", Nombre,
         nombre_usuario,
         Sexo,
         Edad,
@@ -322,7 +274,7 @@ int main(int argc, char * argv[]) {
       }
     }
 
-    fclose(fp);
+    fclose(fp5);
 
   }
   sqlite3_close(db);
