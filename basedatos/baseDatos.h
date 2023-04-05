c6a6523d2222aa00f7cca4a94eeb190475c4c046
sabinluja <sabin.luja@opendeusto.es>
2023-04-05 22:45:37 +0200

    bd

--- basedatos/baseDatos.h
+++ basedatos/baseDatos.h
@@ -6,7 +6,7 @@ int dbExiste(char* fichero);
 int abrirConexion();
 void cerrarConexion(sqlite3* baseDatos);
 void clearIfNeeded(char *str, int max_line);
-int eliminarBD();
+int reiniciarBD();
 
 int gestionarError(sqlite3* baseDatos);
 void gestionarFree(char* str);
