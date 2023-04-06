5425addceb180b60731b9ad6a83ec65aac3ae239
sabinluja <sabin.luja@opendeusto.es>
2023-04-06 22:25:58 +0200

    arreglo

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <locale.h>
 #include "inicio.h"
 #include "basedatos/baseDatos.h"
-
+ 
 //PARA EJECUTAR ESTO:
 
 // gcc main.c inicio.c reservar_local.c comprar_entradas.c basedatos/baseDatos.c basedatos/sqlite/sqlite3.c servidor/inicio_servidor.c -o salida.exe
