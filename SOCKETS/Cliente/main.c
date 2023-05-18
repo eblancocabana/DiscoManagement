d10602774540f243eef4e6fdf85e2dd74f6b94ed
Endika <e.blanco@opendeusto.es>
2023-05-18 20:48:32 +0200

    mains con sockets

--- SOCKETS/Cliente/main.c
+++ SOCKETS/Cliente/main.c
@@ -3,12 +3,15 @@
 #include "inicio.h"
 #include "basedatos/baseDatos.h"
  
-//PARA EJECUTAR ESTO:
+// IMPORTANT: Winsock Library ("ws2_32") should be linked
 
-// gcc main.c inicio.c reservar_local.c comprar_entradas.c seleccion_entradas.c basedatos/baseDatos.c basedatos/sqlite/sqlite3.c servidor/inicio_servidor.c -o salida.exe
+#include <stdio.h>
+#include <winsock2.h>
 
-int main() {
-    setlocale(LC_ALL, "en_US.UTF-8");
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 6000
+
+int main(int argc, char *argv[]) {
 
     printf("\n\n\n\n");
     printf("\e[34m\e[1m");
@@ -20,8 +23,77 @@ int main() {
     printf("\e[0m");
     printf("\n"); 
 
+	WSADATA wsaData;
+	SOCKET s;
+	struct sockaddr_in server;
+	char sendBuff[512], recvBuff[512];
+
+	printf("\nInitialising Winsock...\n");
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return -1;
+	}
+
+	printf("Initialised.\n");
+
+	//SOCKET creation
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+		printf("Could not create socket : %d", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Socket created.\n");
+
+	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(SERVER_PORT);
+
+	//CONNECT to remote server
+	if (connect(s, (struct sockaddr*) &server, sizeof(server)) == SOCKET_ERROR) {
+		printf("Connection error: %d", WSAGetLastError());
+		closesocket(s);
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Connection stablished with: %s (%d)\n", inet_ntoa(server.sin_addr),
+			ntohs(server.sin_port));
+
+
+    
     inicializacion();
     login();
 
-    return 0;
+
+
+
+	// SEND and RECEIVE data
+	printf("Sending message 1... \n");
+	strcpy(sendBuff, "Hello, server.");
+	send(s, sendBuff, sizeof(sendBuff), 0);
+
+	printf("Receiving message 1... \n");
+	recv(s, recvBuff, sizeof(recvBuff), 0);
+	printf("Data received: %s \n", recvBuff);
+
+	printf("Sending message 2... \n");
+	strcpy(sendBuff, "Hello again.");
+	send(s, sendBuff, sizeof(sendBuff), 0);
+	printf("Data sent: %s \n", sendBuff);
+
+	printf("Receiving message 2... \n");
+	recv(s, recvBuff, sizeof(recvBuff), 0);
+	printf("Data received: %s \n", recvBuff);
+
+	printf("Sending last message... \n");
+	strcpy(sendBuff, "Bye");
+	send(s, sendBuff, sizeof(sendBuff), 0);
+	printf("Data sent: %s \n", sendBuff);
+
+	// CLOSING the socket and cleaning Winsock...
+	closesocket(s);
+	WSACleanup();
+
+	return 0;
 }
--- SOCKETS/Servidor/main.c
+++ SOCKETS/Servidor/main.c
@@ -1,15 +1,12 @@
+// IMPORTANT: Winsock Library ("ws2_32") should be linked
+
 #include <stdio.h>
-#include <locale.h>
-#include "inicio.h"
-#include "basedatos/baseDatos.h"
- 
-//PARA EJECUTAR ESTO:
+#include <winsock2.h>
 
-// gcc main.c inicio.c reservar_local.c comprar_entradas.c seleccion_entradas.c basedatos/baseDatos.c basedatos/sqlite/sqlite3.c servidor/inicio_servidor.c -o salida.exe
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 6000
 
-int main() {
-    setlocale(LC_ALL, "en_US.UTF-8");
-
+int main(int argc, char *argv[]) {
     printf("\n\n\n\n");
     printf("\e[34m\e[1m");
     printf("********************************\n");
@@ -20,8 +17,92 @@ int main() {
     printf("\e[0m");
     printf("\n"); 
 
-    inicializacion();
-    login();
+	WSADATA wsaData;
+	SOCKET conn_socket;
+	SOCKET comm_socket;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	char sendBuff[512], recvBuff[512];
 
-    return 0;
+	printf("\nInitialising Winsock...\n");
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return -1;
+	}
+
+	printf("Initialised.\n");
+
+	//SOCKET creation
+	if ((conn_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+		printf("Could not create socket : %d", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Socket created.\n");
+
+	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(SERVER_PORT);
+
+	//BIND (the IP/port with socket)
+	if (bind(conn_socket, (struct sockaddr*) &server,
+			sizeof(server)) == SOCKET_ERROR) {
+		printf("Bind failed with error code: %d", WSAGetLastError());
+		closesocket(conn_socket);
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Bind done.\n");
+
+	//LISTEN to incoming connections (socket server moves to listening mode)
+	if (listen(conn_socket, 1) == SOCKET_ERROR) {
+		printf("Listen failed with error code: %d", WSAGetLastError());
+		closesocket(conn_socket);
+		WSACleanup();
+		return -1;
+	}
+
+	//ACCEPT incoming connections (server keeps waiting for them)
+	printf("Waiting for incoming connections...\n");
+	int stsize = sizeof(struct sockaddr);
+	comm_socket = accept(conn_socket, (struct sockaddr*) &client, &stsize);
+	// Using comm_socket is able to send/receive data to/from connected client
+	if (comm_socket == INVALID_SOCKET) {
+		printf("accept failed with error code : %d", WSAGetLastError());
+		closesocket(conn_socket);
+		WSACleanup();
+		return -1;
+	}
+	printf("Incomming connection from: %s (%d)\n", inet_ntoa(client.sin_addr),
+			ntohs(client.sin_port));
+
+	// Closing the listening sockets (is not going to be used anymore)
+	closesocket(conn_socket);
+
+	//SEND and RECEIVE data
+	printf("Waiting for incoming messages from client... \n");
+	do {
+		int bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
+		if (bytes > 0) {
+			printf("Receiving message... \n");
+			printf("Data received: %s \n", recvBuff);
+
+			printf("Sending reply... \n");
+			strcpy(sendBuff, "ACK -> ");
+			strcat(sendBuff, recvBuff);
+			send(comm_socket, sendBuff, sizeof(sendBuff), 0);
+			printf("Data sent: %s \n", sendBuff);
+
+			if (strcmp(recvBuff, "Bye") == 0)
+				break;
+		}
+	} while (1);
+
+	// CLOSING the sockets and cleaning Winsock...
+	closesocket(comm_socket);
+	WSACleanup();
+
+	return 0;
 }
