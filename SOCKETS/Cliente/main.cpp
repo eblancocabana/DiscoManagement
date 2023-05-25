54bc241a950de514e55e6b27922cf18f9d5881ce
Endika <e.blanco@opendeusto.es>
2023-05-25 20:41:37 +0200

    fix

--- SOCKETS/Cliente/main.cpp
+++ SOCKETS/Cliente/main.cpp
@@ -14,6 +14,18 @@
 
 SOCKET s;
 
+template <typename T>
+T recibir_datos() {
+    char recvBuff[512];
+    int bytesReceived = recv(s, recvBuff, sizeof(recvBuff), 0);
+    if (bytesReceived > 0) {
+        T data;
+        memcpy(&data, recvBuff, sizeof(T));
+        return data;
+    } else {
+        // Handle error when receiving data
+    }
+}
 
 
 
