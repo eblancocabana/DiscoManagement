394d1e559295faf68776f3f514e8581a50b387a0
Endika <e.blanco@opendeusto.es>
2023-05-25 20:25:55 +0200

    f

--- SOCKETS/Cliente/enviar_datos.h
+++ SOCKETS/Cliente/enviar_datos.h
@@ -1,6 +1,6 @@
 #ifndef ENVIAR_DATOS_H
 #define ENVIAR_DATOS_H
 
-void enviar_datos(char *nombre_funcion, int num_args, ...);
+void enviar_datos(const char *nombre_funcion, int num_args, ...);
 
 #endif
