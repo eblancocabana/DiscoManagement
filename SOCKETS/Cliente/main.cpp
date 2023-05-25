82aaa9d1d02a71a3e2b49044cda1129eda3e76de
Endika <e.blanco@opendeusto.es>
2023-05-25 20:23:17 +0200

    f

--- SOCKETS/Cliente/main.cpp
+++ SOCKETS/Cliente/main.cpp
@@ -30,9 +30,9 @@ T recibir_datos() {
 
 #include <string>
 #include <cstdarg>
-void enviar_datos(const std::string &nombre_funcion, int num_args, ...) {
+void enviar_datos(const char *nombre_funcion, int num_args, ...) {
     char sendBuff[512];
-    sprintf(sendBuff, "%s:", nombre_funcion.c_str());
+    sprintf(sendBuff, "%s:", nombre_funcion);
 
     va_list args;
     va_start(args, num_args);
@@ -61,6 +61,7 @@ void enviar_datos(const std::string &nombre_funcion, int num_args, ...) {
 
     send(s, sendBuff, sizeof(sendBuff), 0);
 }
+
 
 
 
