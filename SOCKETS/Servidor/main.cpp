affb7488aa764edc246c5ea991388a4601036a9a
Endika <e.blanco@opendeusto.es>
2023-05-27 01:14:22 +0200

    prueba

--- SOCKETS/Servidor/main.cpp
+++ SOCKETS/Servidor/main.cpp
@@ -1,18 +1,27 @@
 // IMPORTANT: Winsock Library ("ws2_32") should be linked
 
 #include <iostream>
+
 #include <winsock2.h>
+
 #include <string.h>
+
 #include <sstream>
+
 #include <limits>
+
 #include "clases/reservalocal.h"
 
 extern "C" {
-    #include "basedatos/sqlite/sqlite3.h"
-    #include "basedatos/baseDatos.h"
-    #include "estructuras/entrada_est.h"
-    #include "estructuras/reservarlocal_est.h"
-    }
+  #include "basedatos/sqlite/sqlite3.h"
+
+  #include "basedatos/baseDatos.h"
+
+  #include "estructuras/entrada_est.h"
+
+  #include "estructuras/reservarlocal_est.h"
+
+}
 
 #include "administrador/inicio_servidor.h"
 
@@ -44,32 +53,32 @@ using namespace std;
 //g++ main.o inicio.o sqlite.o bd.o -o servidor.exe -lWs2_32        EJECUTAR EL ARCHIVO
 
 void iniciarSesion() {
-    int existe = 1;
-    char input[MAX_REGISTRO];
+  int existe = 1;
+  char input[MAX_REGISTRO];
 
-    cout << "Usuario: ";
-    cin.getline(input, MAX_NOMBRE_USU);
-    char* us = limpiarInput(input);
+  cout << "Usuario: ";
+  cin.getline(input, MAX_NOMBRE_USU);
+  char * us = limpiarInput(input);
 
-    cout << "Contrasenya: ";
-    cin.getline(input, MAX_CONTRASENYA);
-    char* pa = limpiarInput(input);
+  cout << "Contrasenya: ";
+  cin.getline(input, MAX_CONTRASENYA);
+  char * pa = limpiarInput(input);
 
-    existe = comprobarExistencia(us, pa);
-    int admin = comprobarAdmin(us);
+  existe = comprobarExistencia(us, pa);
+  int admin = comprobarAdmin(us);
 
-    if ((existe == 0) && (admin == 0)) {
-        cout << "\nADMIN ENCONTRADO, accediendo al menu\n";
-        inicioServidor();
+  if ((existe == 0) && (admin == 0)) {
+    cout << "\nADMIN ENCONTRADO, accediendo al menu\n";
+    inicioServidor();
 
-    } else if ((existe == 0)) {
-        cout << "\nUSUARIO NO ADMIN. Acceso denegado\n";
-        cout << "Ejecucion finalizada\n";
+  } else if ((existe == 0)) {
+    cout << "\nUSUARIO NO ADMIN. Acceso denegado\n";
+    cout << "Ejecucion finalizada\n";
 
-        return;
-    } else {
-        // Hacer algo si no se encuentra el usuario o la contraseña es incorrecta
-    }
+    return;
+  } else {
+    // Hacer algo si no se encuentra el usuario o la contraseña es incorrecta
+  }
 }
 
 void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
@@ -80,35 +89,35 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
   // Buffer para enviar el valor devuelto por la función llamada
   char sendBuff[512];
   int pos = 0;
-
+  printf("ESTE ES EL PUTO RECIBIFDIOP %s", nombre_funcion);
   // Llamar a la función correspondiente con sus argumentos y enviar el valor devuelto
   if (strcmp(nombre_funcion, "inicializarUsuarios") == 0) {
     int ret = inicializarUsuarios();
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "inicializarDiasDeFiesta") == 0) {
     int ret = inicializarDiasDeFiesta();
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "inicializarDJ") == 0) {
     int ret = inicializarDJ();
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "inicializarRRPP") == 0) {
     int ret = inicializarRRPP();
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "inicializarListaEventos") == 0) {
     int ret = inicializarListaEventos();
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -116,7 +125,7 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     printf("Llamando a inicializacion\n");
     int ret = inicializacion();
     printf("Retorno de inicializacion: %d\n", ret);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -137,19 +146,19 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     pos += size;
   } else if (strcmp(nombre_funcion, "comprobarCodigoLocal") == 0) {
     int ret = comprobarCodigoLocal(args);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "comprobarCodigoRRPP") == 0) {
     int ret = comprobarCodigoRRPP(args);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "comprobarUsuario") == 0) {
     int ret = comprobarUsuario(args);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -162,7 +171,7 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     pos += sizeof(ret);
   } else if (strcmp(nombre_funcion, "comprobarAdmin") == 0) {
     int ret = comprobarAdmin(args);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -171,13 +180,13 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     char * username = strtok(args, ",");
     char * password = strtok(NULL, ",");
     int ret = comprobarExistencia(username, password);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
   } else if (strcmp(nombre_funcion, "comprobarEntrada") == 0) {
     int ret = comprobarEntrada(args);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -187,7 +196,7 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     char * nomDiscoteca = strtok(NULL, ",");
     char * eventoEsp = strtok(NULL, ",");
     int ret = insertarDiaFiesta(fecha, nomDiscoteca, eventoEsp);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -207,7 +216,7 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     printf(contra);
     printf("FUERA");
     int ret = insertarRegistro(nombre, usuario, sexo, edad, correo, contra);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -217,7 +226,7 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     char * nombreDisco = strtok(NULL, ",");
     char * descripcionEvento = strtok(NULL, ",");
     int ret = insertarEvento(fecha, nombreDisco, descripcionEvento);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
@@ -231,173 +240,177 @@ void deserializar_y_llamar_funcion(SOCKET comm_socket, char * recvBuff) {
     char * cvvTarjeta = strtok(NULL, ",");
     char * caducidadTarjeta = strtok(NULL, ",");
     int ret = insertarReservaLocal(codigo_aux, fecha_loc, nombreDiscotec, aforo, numeroTarjeta, cvvTarjeta, caducidadTarjeta);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
-    pos += size; 
+    pos += size;
   } else if (strcmp(nombre_funcion, "mostrarlistadoeventos") == 0) {
     mostrarlistadoeventos(comm_socket);
   } else if (strcmp(nombre_funcion, "mostrarFiestas") == 0) {
     printf("ANTES DE ENTRAR FUNCION\n");
     mostrarFiestas(comm_socket);
     printf("DESPUES DE ENTRAR FUNCION\n");
-  }else if (strcmp(nombre_funcion, "inicicializacionSOCKET") == 0) {
+  } else if (strcmp(nombre_funcion, "inicicializacionSOCKET") == 0) {
     int ret = 1;
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
-  }else if (strcmp(nombre_funcion, "mostrarRRPP") == 0) {
+  } else if (strcmp(nombre_funcion, "mostrarRRPP") == 0) {
     mostrarRRPP(comm_socket);
-  }else if (strcmp(nombre_funcion, "mostrarLocales") == 0) {
+  } else if (strcmp(nombre_funcion, "mostrarLocales") == 0) {
     mostrarLocales(comm_socket);
-  }else if (strcmp(nombre_funcion, "comprobarCodigoEntrada") == 0) {
+  } else if (strcmp(nombre_funcion, "comprobarCodigoEntrada") == 0) {
     int ret = comprobarEntrada(args);
-    const char* rret = (std::to_string(ret)).c_str();
+    const char * rret = (std::to_string(ret)).c_str();
     size_t size = sizeof(rret) + 1;
     memcpy(sendBuff + pos, rret, size);
     pos += size;
-  } else if(strcmp(nombre_funcion,"print:")){
+  } else if (strcmp(nombre_funcion, "print:")) {
     memcpy(sendBuff + pos, "control", sizeof("control"));
     size_t size = sizeof("control") + 1;
     pos += sizeof("control");
+  } else {
+    printf("CULOPENE");
+    send(comm_socket, sendBuff, 0, 0);
+    return;
   }
-  printf("ESTEEE:%s,|%d", sendBuff,sendBuff);
+  printf("ESTEEE:%s,|%d", sendBuff, sendBuff);
   printf("Sending %.*s through socket\n", pos, sendBuff);
   // Enviar el valor devuelto por la función llamada a través del socket
   send(comm_socket, sendBuff, pos, 0);
 }
 
+int main(int argc, char * argv[]) {
+  cout << "\n\n\n\n";
+  cout << "\e[34m\e[1m";
+  cout << "********************************\n";
+  cout << "*                              *\n";
+  cout << "*   SOUND STRATEGY PARTNERS    *\n";
+  cout << "*                              *\n";
+  cout << "********************************\n";
+  cout << "\e[0m";
+  cout << "\n";
 
-int main(int argc, char *argv[]) {
-    cout << "\n\n\n\n";
-    cout << "\e[34m\e[1m";
-    cout << "********************************\n";
-    cout << "*                              *\n";
-    cout << "*   SOUND STRATEGY PARTNERS    *\n";
-    cout << "*                              *\n";
-    cout << "********************************\n";
-    cout << "\e[0m";
-    cout << "\n";
+  WSADATA wsaData;
+  SOCKET conn_socket;
+  SOCKET comm_socket;
+  struct sockaddr_in server;
+  struct sockaddr_in client;
+  char sendBuff[512], recvBuff[512];
 
-    WSADATA wsaData;
-    SOCKET conn_socket;
-    SOCKET comm_socket;
-    struct sockaddr_in server;
-    struct sockaddr_in client;
-    char sendBuff[512], recvBuff[512];
+  int opcion = 0;
+  std::string entrada;
 
-    int opcion = 0;
-    std::string entrada;
+  while (opcion != 1 && opcion != 2) {
+    cout << "Ingrese una opcion\n";
+    cout << "\t1. Acceder como admin modo local\n";
+    cout << "\t2. Ejecutar el servidor\n";
 
-    while (opcion != 1 && opcion != 2) {
-        cout << "Ingrese una opcion\n";
-        cout << "\t1. Acceder como admin modo local\n";
-        cout << "\t2. Ejecutar el servidor\n";
+    getline(cin, entrada);
 
-        getline(cin, entrada);
+    try {
+      opcion = std::stoi(entrada);
 
-        try {
-            opcion = std::stoi(entrada);
+      if (opcion == 1) {
+        cout << "Accediendo como admin modo local...\n";
+        inicializacion();
+        iniciarSesion();
 
-            if (opcion == 1) {
-                cout << "Accediendo como admin modo local...\n";
-                inicializacion();
-                iniciarSesion();
+        return 0;
+      } else if (opcion == 2) {
+        cout << "Ejecutando el servidor para ponerse en escucha...\n";
+      } else {
+        cout << "Opcion no valida. Por favor, intente nuevamente.\n";
+      }
+    } catch (std::exception
+      const & e) {
+      cout << "Entrada no valida. Por favor, ingrese un numero entero.\n";
+    }
+  }
 
-                return 0;
-            } else if (opcion == 2) {
-                cout << "Ejecutando el servidor para ponerse en escucha...\n";
-            } else {
-                cout << "Opcion no valida. Por favor, intente nuevamente.\n";
-            }
-        } catch (std::exception const& e) {
-            cout << "Entrada no valida. Por favor, ingrese un numero entero.\n";
-        }
+  cout << "\nInitialising Winsock...\n";
+  if (WSAStartup(MAKEWORD(2, 2), & wsaData) != 0) {
+    cout << "Failed. Error Code : " << WSAGetLastError();
+
+    return -1;
+  }
+
+  cout << "Initialised.\n";
+
+  //SOCKET creation
+  if ((conn_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    cout << "Could not create socket : " << WSAGetLastError();
+    WSACleanup();
+
+    return -1;
+  }
+
+  cout << "Socket created.\n";
+
+  server.sin_addr.s_addr = inet_addr(SERVER_IP);
+  server.sin_family = AF_INET;
+  server.sin_port = htons(SERVER_PORT);
+
+  //BIND (the IP/port with socket)
+  if (bind(conn_socket, (struct sockaddr * ) & server,
+      sizeof(server)) == SOCKET_ERROR) {
+    cout << "Bind failed with error code: " << WSAGetLastError();
+    closesocket(conn_socket);
+    WSACleanup();
+
+    return -1;
+  }
+
+  cout << "Bind done.\n";
+
+  //LISTEN to incoming connections (socket server moves to listening mode)
+  if (listen(conn_socket, 1) == SOCKET_ERROR) {
+    cout << "Listen failed with error code: " << WSAGetLastError();
+    closesocket(conn_socket);
+    WSACleanup();
+
+    return -1;
+  }
+
+  //ACCEPT incoming connections (server keeps waiting for them)
+  cout << "Waiting for incoming connections...\n";
+  int stsize = sizeof(struct sockaddr);
+  comm_socket = accept(conn_socket, (struct sockaddr * ) & client, & stsize);
+  // Using comm_socket is able to send/receive data to/from connected client
+  if (comm_socket == INVALID_SOCKET) {
+    cout << "accept failed with error code : " << WSAGetLastError();
+    closesocket(conn_socket);
+    WSACleanup();
+
+    return -1;
+  }
+  cout << "Incomming connection from: " << inet_ntoa(client.sin_addr) <<
+    "(" << ntohs(client.sin_port) << ")" << std::endl;
+
+  // Closing the listening sockets (is not going to be used anymore)
+  closesocket(conn_socket);
+
+  //SEND and RECEIVE data
+  cout << "Waiting for incoming messages from client... \n";
+  do {
+    int bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
+    if (bytes > 0) {
+      cout << "Receiving message... \n";
+      cout << "Data received: " << recvBuff << std::endl;
+
+      // Deserializar los datos recibidos y llamar a la función correspondiente con sus argumentos
+      deserializar_y_llamar_funcion(comm_socket, recvBuff);
+
+      if (strcmp(recvBuff, "Bye") == 0)
+        break;
     }
 
-    cout << "\nInitialising Winsock...\n";
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cout << "Failed. Error Code : " << WSAGetLastError();
+  } while (1);
 
-        return -1;
-    }
+  // CLOSING the sockets and cleaning Winsock...
+  closesocket(comm_socket);
+  WSACleanup();
 
-    cout << "Initialised.\n";
-
-    //SOCKET creation
-    if ((conn_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        cout << "Could not create socket : " << WSAGetLastError();
-        WSACleanup();
-
-        return -1;
-    }
-
-    cout << "Socket created.\n";
-
-    server.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(SERVER_PORT);
-
-    //BIND (the IP/port with socket)
-    if (bind(conn_socket, (struct sockaddr*) &server,
-            sizeof(server)) == SOCKET_ERROR) {
-        cout << "Bind failed with error code: " << WSAGetLastError();
-        closesocket(conn_socket);
-        WSACleanup();
-
-        return -1;
-    }
-
-    cout << "Bind done.\n";
-
-    //LISTEN to incoming connections (socket server moves to listening mode)
-    if (listen(conn_socket, 1) == SOCKET_ERROR) {
-        cout << "Listen failed with error code: " << WSAGetLastError();
-        closesocket(conn_socket);
-        WSACleanup();
-
-        return -1;
-    }
-
-    //ACCEPT incoming connections (server keeps waiting for them)
-    cout << "Waiting for incoming connections...\n";
-    int stsize = sizeof(struct sockaddr);
-    comm_socket = accept(conn_socket, (struct sockaddr*) &client, &stsize);
-    // Using comm_socket is able to send/receive data to/from connected client
-    if (comm_socket == INVALID_SOCKET) {
-        cout << "accept failed with error code : " << WSAGetLastError();
-        closesocket(conn_socket);
-        WSACleanup();
-
-        return -1;
-    }
-    cout << "Incomming connection from: " << inet_ntoa(client.sin_addr) <<
-            "(" << ntohs(client.sin_port) << ")"<<std::endl;
-
-     // Closing the listening sockets (is not going to be used anymore)
-     closesocket(conn_socket);
-
-     //SEND and RECEIVE data
-     cout<<"Waiting for incoming messages from client... \n";
-     do {
-         int bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-         if (bytes > 0) {
-             cout<<"Receiving message... \n";
-             cout<<"Data received: "<<recvBuff<<std::endl;
-
-             // Deserializar los datos recibidos y llamar a la función correspondiente con sus argumentos
-            deserializar_y_llamar_funcion(comm_socket, recvBuff);
-
-             if (strcmp(recvBuff, "Bye") == 0)
-                 break;
-         }
-         
-     } while (1);
-
-     // CLOSING the sockets and cleaning Winsock...
-     closesocket(comm_socket);
-     WSACleanup();
-
-     return 0;
+  return 0;
 }
