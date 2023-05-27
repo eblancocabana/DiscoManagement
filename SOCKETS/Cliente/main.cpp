71be3371e58ca03d59ca0e4a9928fe742c1b3372
Endika <e.blanco@opendeusto.es>
2023-05-27 11:59:43 +0200

    fix

--- SOCKETS/Cliente/main.cpp
+++ SOCKETS/Cliente/main.cpp
@@ -47,7 +47,6 @@ char * enviar_datos_char(const char * nombre_funcion, int num_args, ...) {
         char * resultado = new char[bytes_recibidos + 1];
         memcpy(resultado, buffer, bytes_recibidos);
         resultado[bytes_recibidos] = '\0';
-        printf("%s\n", resultado);
         return resultado;
     } else {
         std::cerr << "Error al recibir datos del servidor" << std::endl;
